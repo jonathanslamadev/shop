98ba189f15936b1e4aab378f2fa2e591e827695b
jonathanslama <slamajonathan@gmail.com>
2018-12-17 13:29:19 +0100

    ajout de l'app de contact et de son formulaire.

---
+++ shop_project/contact_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+	subject = models.CharField(max_length=264)
+	email = models.EmailField()
+	text = models.TextField()
+ 
+  def __str__(self):
+    return self.subject
+
+  def __repr__(self):
+    return "<Contact {}>".format(self.subject)
